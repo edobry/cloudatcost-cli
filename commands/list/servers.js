917b3323b8f5703e1307309cc31e45bee88b0b41
Eugene Dobry <edory@gmail.com>
2016-07-11 23:44:11 -0400

    refactored servers, added password viewing

--- commands/list/servers.js
+++ commands/list/servers.js
@@ -3,47 +3,36 @@ var Command = require("ronin").Command,
     util = require("../../util.js"),
     constants = require("../../constants.js");
 
-var formatServers = function(servers) {
-    return servers.map(function(server) {
-        var out = [];
+var annotatedValue = (primary, secondary) => `${primary} (${secondary})`;
+var resourceUsage = (resource) => (used, total) =>`${used}/${total} ${resource}s in use`;
+var gbUsage = resourceUsage("GB");
+var ramUsage = server => gbUsage(server.ramusage, server.ram);
+var hdUsage = server => gbUsage(server.hdusage, server.storage);
 
-        out.push("ID: " + server.id);
+var serverView = (server, showPasswords) =>`
+ID: ${server.id}
+${server.label ? annotatedValue(server.label, server.servername) : server.servername }
+${server.hostname != "Not Assigned" ? annotatedValue(server.hostname, server.ip) : server.ip }
+OS: ${server.template}
+Root password: ${showPasswords ? server.rootpass : ""}
+${server.cpu} CPUs ${server.cpuusage}% in use
+RAM: ${ramUsage(server)}
+Storage: ${hdUsage(server)}
+Status: ${server.status}`;
 
-        if(server.label)
-            out.push(server.label + ' (' + server.servername + ')');
-        else
-            out.push(server.servername);
-
-        if(server.hostname != "Not Assigned")
-            out.push(server.hostname + ' (' + server.ip + ')');
-        else
-            out.push(server.ip);
-
-        out.push("OS: " + server.template);
-
-        out.push(server.cpu + " CPUs: " + server.cpuusage + "% in use");
-
-        out.push("RAM: " + server.ramusage + '/' + server.ram + " GBs in use");
-
-        out.push("Storage: " + server.hdusage + '/' + server.storage + " GBs in use");
-
-        out.push("Status: " + server.status);
-
-        return out;
-    }).map((rows) => rows.join('\n'));
-};
+var formatServers = (showPasswords, servers) => servers.map(serverView).join('\n');
 
 module.exports = Command.extend({
     desc: "Lists servers",
-    run: function () {
-        api.listServers(function(err, res) {
+    options: { showPasswords: "boolean" },
+    run: showPasswords =>
+        api.listServers((err, res) => {
             if(err)
                 throw err;
 
             console.log("Servers:");
             console.log();
 
-            console.log(formatServers(res.data).join('\n\n'));
-        });
-    }
+            console.log(formatServers(showPasswords, res.data));
+        })
 });
